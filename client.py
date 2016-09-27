39388235f1b73a3096baa05ec05488340a452c2b
gilleslabs <gilles.labs.tosi@gmail.com>
2016-09-27 22:30:32 +0200

    Added keepalive capabilities

--- client.py
+++ client.py
@@ -1,11 +1,14 @@
 import socket
 
-host = "localhost"
+host = "localhost" # Replace by server IP address
 port = 12800
 
 connection_to_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 connection_to_server.connect((host, port))
 print("Connection established with server on port {}".format(port))
+
+# Uncomment below line to enable tcp keepalive from server side
+#connection_to_server.ioctl(socket.SIO_KEEPALIVE_VALS, (1, 10000, 3000))
 
 msg_to_send = b""
 while msg_to_send != b"close":
--- server.py
+++ server.py
@@ -8,7 +8,8 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((host, port))
 server_socket.listen(5)
 print("Server listening on {}".format(port))
-
+# Uncomment below line to enable keepalive from server side
+#server_socket.ioctl(socket.SIO_KEEPALIVE_VALS, (1, 10000, 3000))
 client_connection, connection_info = server_socket.accept()
 
 client_data = b""
@@ -16,7 +17,7 @@ while client_data != b"close":
     client_data = client_connection.recv(1024)
     # On client side use only a-Z character in order to avoid error msg
     print(client_data.decode())
-    time.sleep(10)  # Delay for in seconds
+    time.sleep(10)  # Delay for reply in seconds (idle time)
     client_connection.send(b"Data received 5/5")
 
 print("Closing connection")
